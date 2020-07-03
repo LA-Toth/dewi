3529da91aadd979d863b7e8f8e6ba34f7a492cb1
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2020-07-03 23:31:34 +0200

    Updated for newer dewi-core

--- dewi-local.py
+++ dewi-local.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-# Copyright 2015-2019 Laszlo Attila Toth
+# Copyright 2015-2020 Laszlo Attila Toth
 # Distributed under the terms of the GNU Lesser General Public License v3
 
 import sys
 
-from dewi_core.application import MainApplication
+from dewi_core.application import Application
 from dewi_core.loader.loader import PluginLoader
 
 
@@ -12,7 +12,7 @@ def main():
     args = sys.argv[1:]
 
     loader = PluginLoader()
-    app = MainApplication(loader, 'dewi', fallback_to_plugin_name='dewi.DewiPlugin')
+    app = Application(loader, 'dewi', fallback_to_plugin_name='dewi.DewiPlugin')
     app.run(args)
 
 
