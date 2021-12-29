8d17a145876ec83e827cca2be3f847471b0e85bd
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2021-12-29 22:42:24 +0100

    Updated to work with Python 3.10

--- dewi/__init__.py
+++ dewi/__init__.py
@@ -1,17 +1,16 @@
-# Copyright 2015-2019 Laszlo Attila Toth
+# Copyright 2015-2021 Laszlo Attila Toth
 # Distributed under the terms of the GNU Lesser General Public License v3
 
-import collections
+import typing
 
 from dewi_core.loader.context import Context
 from dewi_core.loader.plugin import Plugin
 
 
 class DewiPlugin(Plugin):
-    def get_description(self) -> str:
-        return "DEWI application plugin"
+    """DEWI application plugin"""
 
-    def get_dependencies(self) -> collections.Iterable:
+    def get_dependencies(self) -> typing.Iterable[str]:
         return {'dewi_commands.CommandsPlugin'}
 
     def load(self, c: Context):
